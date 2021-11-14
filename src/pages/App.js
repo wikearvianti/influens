4a99ec7981707b0be680720daefc94ac3a216019
diarih <diarihza@gmail.com>
2021-11-14 19:12:56 +0700

    Adding Context API with Auth, Adding Context to index js, Adding Context to App js, Adding Context to Landing Page, Fixing at modal and Form Pengajuan

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,19 @@
+import { useAuthDispatch, useAuthState } from "../Context/auth/auth";
+import { useEffect } from "react";
+import { fetchApi } from "../Context/action/action";
+
 function App() {
+
+  const dispatch = useAuthDispatch()
+  const state = useAuthState()
+  console.log(state);
+
+  useEffect(()=>{
+    fetchApi(dispatch);
+  }, [dispatch])
+
+  
+
   return (
     <div className="App">
     </div>
