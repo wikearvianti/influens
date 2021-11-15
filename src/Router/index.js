9b117f63804ca2a264afda9bfc0fccc7d27e9130
diarih <diarihza@gmail.com>
2021-11-15 22:52:18 +0700

    anything

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import App from '../pages/App'
+import LandingPage from "../pages/Landing-Page/LandingPage"
 import FormPengajuan from "../pages/Form-Pengajuan/Form-pengajuan";
 import Profile from "../pages/Profile/Profile"
 
 export default function Routers() {
     return(
         <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/form-pengajuan" element={<FormPengajuan />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/form-pengajuan/:id" element={<FormPengajuan />} />
             <Route path="/profile" element={<Profile />} />
         </Routes>
     )
