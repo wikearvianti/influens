b65782c4d8476c7bf60a1e5ec02759bdf1e6042c
dewimardanic <dewimardanic@gmail.com>
2021-11-19 00:16:12 +0800

    edit card

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import "./login.css";
 import Layouts from "../../Layout";
 
